24c13495817b7ab066b24ecbd615b1c72b654c75
Diego Cardalliaguet <diego.cgm@es.ibm.com>
2020-12-09 14:59:23 +0100

    Update twinkleTree.py

--- twinkleTree.py
+++ twinkleTree.py
@@ -16,10 +16,7 @@ sense.clear()
 
 X = [0, 100, 0] # Green
 O = [0, 0, 0] # Empty
-W = [150, 150, 150]  # White
 B = [139, 69, 19] # Brown
-U = [0, 0, 128] # Blue
-R = [255, 0, 0] # Red
 
 try:
     while True:
@@ -27,14 +24,14 @@ try:
 	        O, O, O, random_color(), X, O, O, O,
 	        O, O, X, X, X, random_color(), O, O,
 	        O, random_color(), X, X, random_color(), X, X, O,
-	        O, O, X, X, random_color(), X, O, O,
+	        O, O, X, random_color(), X, X, O, O,
 	        O, X, random_color(), X, X, random_color(), X, O,
 	        X, random_color(), X, random_color(), X, X, random_color(), X,
 	        O, O, O, B, B, O, O, O,
 	        O, O, O, B, B, O, O, O
 	        ]
-        sense.set_pixels(xmas_tree) #pixels, not pixel as in the former sentence.
+        sense.set_pixels(xmas_tree) 
         sleep(2)
 except KeyboardInterrupt:
     sense.clear()
-    print('Stopped')
+    print('\n Stopped')
